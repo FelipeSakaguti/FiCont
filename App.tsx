8d16af826daa08bc5bdb4c75a9762d33a167eaa5
unknown <felipesakaguti@hotmail.com>
2021-12-09 16:17:59 -0300

    Add Pagina Resumo

--- App.tsx
+++ App.tsx
@@ -1,4 +1,7 @@
+import 'react-native-gesture-handler'
+
 import React from 'react';
+import { StatusBar } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
@@ -31,6 +34,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme} >
       <NavigationContainer>
+        <StatusBar barStyle={'light-content'} />
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
